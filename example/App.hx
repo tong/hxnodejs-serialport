0814e0ac903f81220e983a4041cf137c7555a224
tong <tong@disktree.net>
2018-07-16 13:30:45 +0200

    Update example application

--- example/App.hx
+++ example/App.hx
@@ -6,26 +6,30 @@ class App {
 
 	static function main() {
 
-		SerialPort.list(function(e,devices) {
+		Sys.println( 'Checking for connected serial devices …' );
+
+		SerialPort.list( function(e,devices) {
 
 			if( e != null ) trace(e) else {
 
-				for( device in devices ) trace( device );
+				Sys.println( 'Found ${devices.length} connected serial devices' );
+				for( i in 0...devices.length ) Sys.println( '$i: ${devices[i]}' );
 
 				var port = devices[0];
 				var serial = new SerialPort( port.comName, {
 					baudRate: _115200
 				});
-				serial.on( open, function(){
-					trace( 'Connected to: '+port.comName );
-					serial.write( new Buffer('ls\n'), function(e) {
+				serial.on( SerialPortEvent.open, function(){
+					Sys.println( 'Connected to [${port.comName}]' );
+					serial.write( new Buffer( 'ls\n' ), function(e) {
 						if( e != null ) trace(e);
 				    });
 				});
-				serial.on( data, function(data){
-					trace( data.toString() );
+				serial.on( SerialPortEvent.data, function(buf){
+					trace( buf.toString() );
 				});
 			}
 		});
 	}
+
 }
