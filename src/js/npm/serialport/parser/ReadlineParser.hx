0c89c8a68220d641479c9befd6963a21f91fb80f
tong <tong@disktree.net>
2018-11-21 10:56:07 +0100

    Fix

--- src/js/npm/serialport/parser/ReadlineParser.hx
+++ src/js/npm/serialport/parser/ReadlineParser.hx
@@ -2,5 +2,5 @@ package js.npm.serialport.parser;
 
 @:jsRequire("@serialport/parser-readline")
 extern class ReadlineParser extends js.node.stream.Transform<ReadlineParser> {
-	function new( options: { delimiter: String, ?encoding : String } ) : Void;
+	function new( ?options: { delimiter: String, ?encoding : String } ) : Void;
 }
