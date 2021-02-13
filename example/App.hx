3597c176ecae49c875d9d041a3b25804e92dd7f3
tong <tong@disktree.net>
2021-02-13 12:27:42 +0100

    Example: use module level fields

--- example/App.hx
+++ example/App.hx
@@ -2,31 +2,27 @@
 import js.node.Buffer;
 import js.npm.serialport.SerialPort;
 
-class App {
+function main() {
 
-	static function main() {
-
-		SerialPort.list().then( function(ports:Array<SerialPortInfo>) {
-
-			Sys.println( 'Found ${ports.length} connected serial ports' );
-			for( i in 0...ports.length ) Sys.println( '$i = ${ports[i]}' );
-
-			var port = new SerialPort( ports[0].path, {
-				baudRate: B115200
-			});
-			port.on( SerialPortEvent.open, function(){
-				Sys.println( 'Connected to [${ports[0].path}]' );
-				port.write( new Buffer( 'ls\n' ), function(e) {
-					if( e != null ) trace(e);
-				});
-			});
-			port.on( SerialPortEvent.data, function(buf){
-				trace( buf.toString() );
-			});
-
-		}).catchError( function(e) {
-			trace(e);
+	SerialPort.list().then( (ports:Array<SerialPortInfo>) -> {
+	
+		Sys.println( 'Found ${ports.length} connected serial ports' );
+		for( i in 0...ports.length ) Sys.println( '$i = ${ports[i]}' );
+	
+		var port = new SerialPort( ports[0].path, {
+			baudRate: B115200
 		});
-	}
-
+		port.on( SerialPortEvent.open, () -> {
+			Sys.println( 'Connected to [${ports[0].path}]' );
+			port.write( new Buffer( 'ls\n' ), e -> {
+				if( e != null ) trace(e);
+			});
+		});
+		port.on( SerialPortEvent.data, buf -> {
+			trace( buf.toString() );
+		});
+	
+	}).catchError( e -> {
+		trace(e);
+	});
 }
